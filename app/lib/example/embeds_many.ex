72b6a23805960cea4c10b7549395731c69f5abc7
toran billups <toranb@gmail.com>
2019-06-18 10:14:59 -0500

    now embeds_many supports nil from get_field

--- app/lib/example/embeds_many.ex
+++ app/lib/example/embeds_many.ex
@@ -6,13 +6,23 @@ defmodule Example.EmbedsMany do
       changeset
       |> cast_embed(field)
 
-    altered_changeset =
+    changes =
       updated_changeset
       |> get_change(field)
-      |> Enum.map(&alter_action/1)
 
     updated_changeset
-    |> put_embed(field, altered_changeset)
+      |> update_embed(field, changes)
+  end
+
+  def update_embed(changeset, _field, nil), do: changeset
+
+  def update_embed(changeset, field, changes) do
+    altered_changesets =
+      changes
+      |> Enum.map(&alter_action/1)
+
+    changeset
+    |> put_embed(field, altered_changesets)
   end
 
   def alter_action(%Ecto.Changeset{action: :replace} = changeset) do
